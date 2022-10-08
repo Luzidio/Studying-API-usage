c3755954e3004718f24e99b430212c2b2aaa7916
enoquetembe <enoquetembetembe10@gmail.com>
2022-10-07 21:56:32 -0300

    show movies

--- Marvel API/script.js
+++ Marvel API/script.js
@@ -12,7 +12,45 @@ const form = document.querySelector('#form');
 getMovies(requestURL);
 function getMovies(url) {
     fetch(url).then(res => res.json()).then(data => {
-        console.log(data.data.results);
+        showMovies(data.data.results);
   
     })
+}
+
+function showMovies(data) {
+
+    
+
+    data.forEach(movie => {
+
+       
+
+        const sourceImg =
+        movie.thumbnail.path + '.' + movie.thumbnail.extension
+
+        const {name, description} = movie
+        const element = document.createElement('div');
+        element.classList.add('character');
+        element.innerHTML = ` 
+        <img src="${sourceImg}">
+
+        <div class="info">
+            <h3 class="name">${name}</h3>
+        </div>
+
+        <div class="description">
+            <h3>Description</h3>
+            <p>
+                ${description};
+            </p>
+        </div>
+        
+        `
+       
+
+        main.appendChild(element);
+
+       
+    });
+
 }
